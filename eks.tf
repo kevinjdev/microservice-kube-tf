ae7ee019d0c4ac54c6983cbdcc9d7d30d5dc0948
Kevin Jaeger <kevin.a.jaeger@gmail.com>
2024-10-13 16:46:15 -0600

    adding node groups

--- eks.tf
+++ eks.tf
@@ -8,22 +8,33 @@ module "eks" {
   enable_cluster_creator_admin_permissions = true
   cluster_endpoint_public_access           = true
 
-  fargate_profiles = {
-    fargate-default = {
-      name = var.fargate_profile_name
-      selectors = [
-        {
-          namespace = var.namespace
-        }
-      ]
-    }
-  }
-
   cluster_addons = {
     coredns                = {}
     eks-pod-identity-agent = {}
     kube-proxy             = {}
     vpc-cni                = {}
+  }
+
+  eks_managed_node_groups = {
+    node_group_one = {
+      name           = "node-group-1"
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+
+    node_group_two = {
+      name           = "node-group-2"
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
   }
 
   tags = var.required_tags
